4b18a113223be2f716e88840add07754f1855c47
Anarion Technologies <sainig273@gmail.com>
2026-02-15 17:16:11 +0530

    Initial Commit

--- backend_bootstrap/outputs.tf
+++ backend_bootstrap/outputs.tf
@@ -1,19 +1,19 @@
 output "s3_bucket_name" {
   description = "Name of the created s3 bucket"
-  value = aws_s3_bucket.mybucket.bucket
+  value       = aws_s3_bucket.mybucket.bucket
 }
 
 output "dynamodb_table_name" {
   description = "Name of the created DynamoDB table"
-  value = aws_dynamodb_table.terraform_locks.name
+  value       = aws_dynamodb_table.terraform_locks.name
 }
 
 output "s3_bucket_arn" {
   description = "ARN of the S3 bucket"
-  value = aws_s3_bucket.mybucket.arn
+  value       = aws_s3_bucket.mybucket.arn
 }
 
 output "dynamodb_table_arn" {
   description = "ARN of the DynamoDB table"
-  value = aws_dynamodb_table.terraform_locks.arn
+  value       = aws_dynamodb_table.terraform_locks.arn
 }
